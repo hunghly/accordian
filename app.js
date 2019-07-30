3b3c8208beb2839e13fa4628def76a4eb4c93e07
Hung Ly <hungly@Hungs-MBP.attlocal.net>
2019-07-30 01:22:06 -0500

    Final Product

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 "use strict"
+const accordionItemEl = document.getElementsByClassName("accordion__item");
+const iconEl = document.querySelector(".accordion__icon");
+
+function accordion() {
+    for (let item of accordionItemEl) {
+        let accHeader = item.firstElementChild;
+        accHeader.addEventListener("click", function () {
+            let accDetails = this.nextElementSibling;
+            if (accDetails.style.maxHeight != 0) {
+                accDetails.style.maxHeight = null;
+                this.lastElementChild.innerHTML = "+";
+            } else {
+                accDetails.style.maxHeight = accDetails.scrollHeight + "px";
+                this.lastElementChild.innerHTML = "-";
+            }
+        })
+    }
+}
+
+accordion();
